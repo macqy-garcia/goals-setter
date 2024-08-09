490da1bceabcea2ce6f19cea2842afa64a63a777
“macqy <garciamacqy@gmail.com>
2024-08-09 14:05:42 +0800

    added api base url

--- src/features/goals/goalService.js
+++ src/features/goals/goalService.js
@@ -1,6 +1,6 @@
-import axios from "axios";
+import axios from 'axios';
 
-const API_URL = "api/goals/";
+const API_URL = `${process.env.API_BASE_URL}/api/goals/`;
 
 //Create new goal
 const createGoal = async (goalData, token) => {
